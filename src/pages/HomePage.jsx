8966610e2db606b91d87f6f9905aa48da6ca59bd
shital Dalavi <shital.dalavi3@gmail.com>
2024-09-10 23:57:23 +0200

    display pokemon cards with name and image

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -24,20 +24,33 @@ const HomePage = () => {
     return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
   };
   return (
-    <div className="relative card w-72 bg-red shadow-md m-4 border group rounded-xl overflow-hidden">
-      {pokemonData.map((pokemon) => (
-        <div key={pokemon.name} className="bg-red p-4 rounded shadow">
-          <h2 className="text-xl font-semibold">{pokemon.name}</h2>
-          <div>{pokemon.url}</div>
-          <img
-            src={getPokemonImageUrl(pokemon.url.split("/")[6])}
-            alt={`${pokemon.name} sprite`}
-            c
-            className="w-full h-72 object-cover transition-opacity duration-300 group-hover:opacity-40"
-          ></img>
+    <>
+      <div className="bg-white">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+          {pokemonData.map((pokemon) => (
+            <div key={pokemon.name} className="bg-red p-4 rounded shadow">
+              <h2 className="text-xl font-semibold text-red-500">
+                {pokemon.name}
+              </h2>
+              {/* <div>{pokemon.url}</div>*/}
+              <img
+                src={getPokemonImageUrl(pokemon.url.split("/")[6])}
+                alt={`${pokemon.name} sprite`}
+                className="w-full h-72 object-cover transition-opacity duration-300 group-hover:opacity-40"
+              ></img>
+              <div className="space-x-3">
+                <button className="btn bg-red-500 text-white px-2 py-1 rounded p-3 ">
+                  play
+                </button>
+                <button className="btn bg-red-300 text-white px-2 py-1 rounded p-3">
+                  more details
+                </button>
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      </div>
+    </>
   );
 };
 
