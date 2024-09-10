368d4206af4337c7f7c76667453c94b2f03fce3b
shital Dalavi <shital.dalavi3@gmail.com>
2024-09-10 22:22:40 +0200

    display name and image for cards

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,73 +2,43 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
-  const [pokemonList, setPokemonList] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [showData, setShowData] = useState(false);
+  const [pokemonData, setPokemonData] = useState([]);
 
   useEffect(() => {
-    const fetchPokemon = async () => {
+    const fetchPokemonData = async () => {
       try {
         const response = await fetch(
           "https://pokeapi.co/api/v2/pokemon?limit=100"
         );
         const data = await response.json();
-        setPokemonList(
-          data.results.map((result) => ({
-            id: result.url.match(/\/([0-9]*)\/$/)[1],
-            name: result.name,
-            image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-              result.url.match(/\/([0-9]*)\/$/)[1]
-            }.png`,
-          }))
-        );
+        setPokemonData(data.results);
       } catch (error) {
-        console.error("Error loading data:", error);
-        setError("Failed to load data");
-      } finally {
-        setLoading(false);
+        console.error("Error fetching Pokémon data:", error);
       }
     };
-    fetchPokemon();
+
+    fetchPokemonData();
   }, []);
 
-  const handleButtononClick = () => {
-    setShowData(showData);
+  const getPokemonImageUrl = (id) => {
+    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
   };
   return (
-    <>
-      <Box p="6">
-        <Heading mb="6" textAlign="center">
-          My Pokémon Collection
-        </Heading>
-        <Flex justifyContent="center">
-          <Button colorScheme="teal" onClick={handleButtonClick}>
-            {showData ? "Hide Pokémon" : "Show Pokémon"}
-          </Button>
-        </Flex>
-        {showData && (
-          <Flex mt="6" flexWrap="wrap" justifyContent="center">
-            {data.map((pokemon) => (
-              <Box key={pokemon.id} color="white" backgroundColor="black">
-                <img src={pokemon.image} alt={pokemon.name} />
-                <p>{pokemon.name}</p>
-              </Box>
-            ))}
-          </Flex>
-        )}
-      </Box>
-      {/*
-      <div className="container mx-auto p-16">
-        <h2 className="text-4xl font-bold mb-6 font-serif  text-secondary">
-          Our Products
-        </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-          {pokemonList.map((pokemon) => (
-            <ProductCard key={pokemon._id} product={product} />
-          ))}
+    <div className="relative card w-72 bg-red shadow-md m-4 border group rounded-xl overflow-hidden">
+      {pokemonData.map((pokemon) => (
+        <div key={pokemon.name} className="bg-red p-4 rounded shadow">
+          <h2 className="text-xl font-semibold">{pokemon.name}</h2>
+          <div>{pokemon.url}</div>
+          <img
+            src={getPokemonImageUrl(pokemon.url.split("/")[6])}
+            alt={`${pokemon.name} sprite`}
+            c
+            className="w-full h-72 object-cover transition-opacity duration-300 group-hover:opacity-40"
+          ></img>
         </div>
-      </div>*/}
-    </>
+      ))}
+    </div>
   );
 };
+
+export default HomePage;
