59c7b933dec3cb021221391d822b2a354e2e5eb9
dhruvi <dhruvi.balar@gmail.com>
2024-09-12 14:34:07 +0200

    add category filter with only one andcombination

--- src/pages/PokemonCategory.jsx
+++ src/pages/PokemonCategory.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import axios from "axios";
 
-const PokemonCategory = () => {
+const PokemonCategory = ({ selectedCategories, onCategoryClick }) => {
   const [types, setTypes] = useState([]);
 
   useEffect(() => {
@@ -19,26 +18,37 @@ const PokemonCategory = () => {
     fetchTypes();
   }, []);
 
+  // Check if a category is selected
+  const isCategorySelected = (category) => selectedCategories.includes(category);
+
   return (
-    <div className="bg-white py-4 ">
+    <div className="bg-white py-4">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-red-500 text-2xl font-bold p-4 px-0">
-          <Link to="/">Categories</Link>
-        </div>
+        <div className="text-red-500 text-2xl font-bold p-4 px-0">Categories</div>
       </div>
       <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-10 gap-4">
+        {/* "All" Button */}
+        <div className="bg-red-100 p-4 rounded shadow">
+          <button
+            onClick={() => onCategoryClick("all")} // Trigger "All" action
+            className={`${
+              selectedCategories.length === 0 ? "bg-red-300" : "text-red-500"
+            } hover:bg-red-200 transition`}
+          >
+            All
+          </button>
+        </div>
+        {/* Category Buttons */}
         {types.map((type) => (
           <div key={type.name} className="bg-red-100 p-4 rounded shadow">
-            {/* <img
-            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/types/${type.name}.png`}
-            alt={`${type.name} type`}
-          />*/}
-            <Link
-              to={`${type.name}`}
-              className="text-red-500  hover:bg-red-200 transition"
+            <button
+              onClick={() => onCategoryClick(type.name)}
+              className={`${
+                isCategorySelected(type.name) ? "bg-red-300" : "text-red-500"
+              } hover:bg-red-200 transition`}
             >
               {type.name}
-            </Link>
+            </button>
           </div>
         ))}
       </div>
