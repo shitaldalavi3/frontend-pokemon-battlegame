42358f48d8e85f2209584ac1bea25abea7c9407f
shital Dalavi <shital.dalavi3@gmail.com>
2024-09-16 13:14:40 +0200

    rename file with name LeaderBoard

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -300,11 +300,9 @@ const HomePage = () => {
                     <button className="bg-red-500 text-white px-4 py-2 rounded shadow hover:bg-red-400 transition">
                       Play
                     </button>
-                    <div className="mt-3 space-x-3">
-                      <button className="bg-red-500 text-white px-4 py-2 rounded shadow hover:bg-red-400 transition">
-                        Add to Roster
-                      </button>
-                    </div>
+                    <button className="bg-red-500 text-white px-4 py-2 rounded shadow hover:bg-red-400 transition">
+                      Add to Roster
+                    </button>
                   </div>
                 </div>
               ))}
